af257736120523cb56fca910fd977c83cab14e5c
Victor Pasenchuk <vpasenchuk@thumbtack.net>
2018-02-04 13:55:40 +0600

    Calling

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,23 +7,29 @@ import org.leanpoker.player.models.PokerPlayer;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "v0.2 calling";
 
     public static int betRequest(JsonElement request) {
         final Game game = new Gson().fromJson(request, Game.class);
         final PokerPlayer me = getMe(game);
-        
-        return 1000;
-    }
 
-    private static PokerPlayer getMe(Game game) {
-        for (PokerPlayer pokerPlayer : game.getPlayers()) {
-            if ("valEty".equals(pokerPlayer.getName()))
-                return pokerPlayer;
-        }
-        return null;
+        return call(game);
     }
 
     public static void showdown(JsonElement game) {
+    }
+
+    private static PokerPlayer getMe(Game game) {
+        return game.getPlayers().get(game.getInAction());
+    }
+
+    private static int call(Game game) {
+        final PokerPlayer me = getMe(game);
+        return game.getCurrentBuyIn() - me.getBet();
+    }
+
+    private static int raise(Game game) {
+        final PokerPlayer me = getMe(game);
+        return game.getCurrentBuyIn() - me.getBet() + game.getMinimumRaise();
     }
 }
