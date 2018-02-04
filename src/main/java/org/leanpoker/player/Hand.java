cc8d5760cf3c09e6b7da6172f81e7973a57ce6ae
Victor Pasenchuk <vpasenchuk@thumbtack.net>
2018-02-04 15:54:37 +0600

    Add probabilities calculator

---
+++ src/main/java/org/leanpoker/player/Hand.java
@@ -0,0 +1,53 @@
+package org.leanpoker.player;
+
+import org.leanpoker.player.models.Game;
+import org.leanpoker.player.models.HoleCard;
+import org.leanpoker.player.models.PokerPlayer;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.leanpoker.player.PokerUtils.getMe;
+
+public class Hand {
+
+    private String card1, card2;
+    private boolean suited;
+    private float[] probabilities;
+
+    public Hand(Game game) {
+        final PokerPlayer me = getMe(game);
+        final List<HoleCard> holeCards = me.getHoleCards();
+        card1 = holeCards.get(0).getRank().replace("10", "T");
+        card2 = holeCards.get(1).getRank().replace("10", "T");
+        suited = holeCards.get(0).getSuit().equals(holeCards.get(1).getSuit());
+    }
+
+    public Hand(String shortDescription, float[] probabilities) {
+        this.probabilities = probabilities;
+        final char[] chars = shortDescription.toCharArray();
+        card1 = String.valueOf(chars[0]);
+        card2 = String.valueOf(chars[1]);
+        suited = chars.length == 3 && chars[2] == 's';
+    }
+
+    public boolean isSuited() {
+        return suited;
+    }
+
+    public float getWinProbability(int numberOfPlayers) {
+        return probabilities[numberOfPlayers - 2];
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Hand) {
+            final Hand other = (Hand) obj;
+            return ((Objects.equals(this.card1, other.card1) && Objects.equals(this.card2, other.card2)) || (
+                    Objects.equals(this.card1, other.card2) && Objects.equals(this.card2, other.card1)))
+                    && this.suited == other.suited;
+
+        }
+        return super.equals(obj);
+    }
+}
--- src/main/java/org/leanpoker/player/PokerUtils.java
+++ src/main/java/org/leanpoker/player/PokerUtils.java
@@ -1,10 +1,58 @@
 package org.leanpoker.player;
 
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
 import org.leanpoker.player.models.Game;
 import org.leanpoker.player.models.HoleCard;
 import org.leanpoker.player.models.PokerPlayer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class PokerUtils {
+    private static List<Hand> preFlopHands = calculatePreFlopHands();
+
+    public static int getActivePlayerCount(Game game) {
+        int count = 0;
+        for (PokerPlayer pokerPlayer : game.getPlayers()) {
+            if ("active".equals(pokerPlayer.getStatus()))
+                count++;
+        }
+        return count;
+    }
+
+    public static float getPreFlopProbability(Game game) {
+        final Hand hand = new Hand(game);
+        for (Hand preFlopHand : preFlopHands) {
+            if (preFlopHand.equals(hand))
+                return preFlopHand.getWinProbability(getActivePlayerCount(game));
+        }
+        return 0;
+    }
+
+    public static List<Hand> getPreFlopHands() {
+        return preFlopHands;
+    }
+
+    private static List<Hand> calculatePreFlopHands() {
+        final LinkedList<Hand> hands = new LinkedList<>();
+        try {
+            final List<String> readLines = FileUtils.readLines(new File("pre flop.txt"), Charsets.UTF_8);
+            for (String line : readLines) {
+                final String[] chunks = line.replace("%", "").replace("\n", "").split("\t");
+                final float[] probabilities = new float[chunks.length - 1];
+                for (int i = 0; i < probabilities.length; i++) {
+                    probabilities[i] = Float.parseFloat(chunks[i + 1]);
+                }
+                hands.add(new Hand(chunks[0], probabilities));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return hands;
+    }
 
     public static int getRank(HoleCard card) {
         switch (card.getRank()) {
