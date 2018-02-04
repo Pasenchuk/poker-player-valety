c257a58f3005dcd87f93e0c79068838d9b53d345
jannoterr <jannoterr@gmail.com>
2018-02-04 13:55:07 +0600

    new version + paired

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,13 +7,27 @@ import org.leanpoker.player.models.PokerPlayer;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "v 0.1";
 
     public static int betRequest(JsonElement request) {
         final Game game = new Gson().fromJson(request, Game.class);
+
         final PokerPlayer me = getMe(game);
-        
-        return 1000;
+        final HoleCard hc1 = me.getHoleCards()[0];
+        final HoleCard hc2 = me.getHoleCards()[1];
+
+        final int currentPot = game.getPot();
+        final int ourBank = me.getStack();
+
+        if (hc1.getSuit() == hc2.getSuit()){
+          return (int)(OurBank*0.5);
+        } else {
+          if (hc1.getRank() == hc2.getRank()) {
+            return (int)(OurBank*0.3);
+          }
+        }
+
+        return 10;
     }
 
     private static PokerPlayer getMe(Game game) {
@@ -26,4 +40,5 @@ public class Player {
 
     public static void showdown(JsonElement game) {
     }
+
 }
