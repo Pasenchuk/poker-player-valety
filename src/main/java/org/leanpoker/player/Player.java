62321da745567a5f2e576a8e3d80c52757fb7faf
jannoterr <jannoterr@gmail.com>
2018-02-04 16:20:29 +0600

    some reorginized logic

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -17,7 +17,13 @@ public class Player {
     public static int betRequest(JsonElement request) {
         final Game game = new Gson().fromJson(request, Game.class);
 
-        return preFlopStrategy(game);
+        final boolean preflopGaming = game.getCommunityCards().size() > 0 ? false : true;
+        if (preflopGaming) {
+          return preFlopStrategy(game);
+        } else {
+          return oldStrategy(game);
+        }
+        // return preFlopStrategy(game);
     }
 
     private static int preFlopStrategy(Game game) {
@@ -38,7 +44,6 @@ public class Player {
 
     }
 
-
     private static int oldStrategy(Game game) {
         final PokerPlayer me = getMe(game);
 
@@ -51,51 +56,51 @@ public class Player {
         final boolean suited = hc1.getSuit().equals(hc2.getSuit());
         final boolean pair = hc1.getRank().equals(hc2.getRank());
 
-        final boolean preflopGaming = game.getCommunityCards().size() > 0 ? false : true;
+//         final boolean preflopGaming = game.getCommunityCards().size() > 0 ? false : true;
+//
+// //        if (suited && pair)
+// //            return raise(game);
+//         if (preflopGaming) {
+//             // PREFLOP only 2 cards on hands
+//
+//             int r1 = getValueRank(hc1);
+//             int r2 = getValueRank(hc2);
+//
+//             if (suited) {
+//                 //            return (int) (ourBank * 0.5);
+//                 if (r1 > 9 && r2 > 9) {
+//                     return call(game);
+//                 } else {
+//                     return 0;
+//                 }
+//             }
+//
+//             if (pair) {
+//                 //            return (int) (ourBank * 0.3);
+//                 if (r1 > 9 && r2 > 9) {
+//                     return ourBank;
+//                 } else {
+//                     Random rand = new Random();
+//                     int value = rand.nextInt(8) + 2;
+//
+//                     if (r1 > value) {
+//                         return call(game);
+//                     }
+//
+//                     return 0;
+//                 }
+//
+//                 // return raise(game);
+//             }
+//             //        return game.getSmallBlind();
+//
+//             if (r1 >= 12 || r2 >= 12) {
+//                 return call(game);
+//             }
+//
+//             return 0;
 
-//        if (suited && pair)
-//            return raise(game);
-        if (preflopGaming) {
-            // PREFLOP only 2 cards on hands
-
-            int r1 = getValueRank(hc1);
-            int r2 = getValueRank(hc2);
-
-            if (suited) {
-                //            return (int) (ourBank * 0.5);
-                if (r1 > 9 && r2 > 9) {
-                    return call(game);
-                } else {
-                    return 0;
-                }
-            }
-
-            if (pair) {
-                //            return (int) (ourBank * 0.3);
-                if (r1 > 9 && r2 > 9) {
-                    return ourBank;
-                } else {
-                    Random rand = new Random();
-                    int value = rand.nextInt(8) + 2;
-
-                    if (r1 > value) {
-                        return call(game);
-                    }
-
-                    return 0;
-                }
-
-                // return raise(game);
-            }
-            //        return game.getSmallBlind();
-
-            if (r1 >= 12 || r2 >= 12) {
-                return call(game);
-            }
-
-            return 0;
-
-        } else {
+        // } else {
             // FLOP, TURN, RIVER
 
             if (game.getCurrentBuyIn() > (0.3 * ourBank)) {
@@ -103,7 +108,7 @@ public class Player {
             }
 
             return call(game);
-        }
+        // }
     }
 
     public static void showdown(JsonElement game) {
